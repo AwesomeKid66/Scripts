ffe66853d81b36226e941fc03bb82801e32263d3
AwesomeKid66 <mj3.14okrzesik@sbcglobal.net>
2025-08-27 09:06:11 -0500

    fix: window doesn't close

--- scripts/remove_power_awake_devices.ps1
+++ scripts/remove_power_awake_devices.ps1
@@ -1,11 +1,12 @@
-# Self-elevating PowerShell script
+# Self-elevating PowerShell script to disable all wake devices except power button
 # Save as disable-wake.ps1
 
 # Check if running as Administrator
 $currUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 if (-not $currUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Write-Host "Restarting script with administrator privileges..." -ForegroundColor Yellow
-    Start-Process powershell "-ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    # Relaunch with elevation and keep window open (-NoExit)
+    Start-Process powershell "-NoExit -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     exit
 }
 
