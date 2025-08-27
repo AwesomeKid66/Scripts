7bfbca7a3c5491af2dc0f0a020a4840cb8a68cad
AwesomeKid66 <mj3.14okrzesik@sbcglobal.net>
2025-08-27 09:02:51 -0500

    fix: updated code to run as administrator

--- scripts/remove_power_awake_devices.ps1
+++ scripts/remove_power_awake_devices.ps1
@@ -1,5 +1,17 @@
-# Disable all devices that are currently allowed to wake the computer
-# except the system power button itself.
+# Self-elevating PowerShell script
+# Save as disable-wake.ps1
+
+# Check if running as Administrator
+$currUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if (-not $currUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "Restarting script with administrator privileges..." -ForegroundColor Yellow
+    Start-Process powershell "-ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
+# ------------------------
+# Main script logic below
+# ------------------------
 
 # Get list of wake-enabled devices
 $wakeDevices = powercfg -devicequery wake_armed
